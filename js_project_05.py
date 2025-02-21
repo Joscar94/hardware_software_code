27c123dd1164230a9a195407baf7c9e8fca61978
Joscar94 <joscarsanchez2@gmail.com>
2025-02-20 21:06:15 -0500

    2/20 hardware software classwork

--- js_project_05.py
+++ js_project_05.py
@@ -5,4 +5,11 @@ def conversation():
     print("That's a good number")
     print("Enter another number")
     print("That's a great number")
-    
+    print("Let's do some adding")
+    print("Type 'yes' to end program")
+    print("Enter first number")
+    print("Invalid number, try again")
+    print("Type 'yes' to end program")
+    print("Thank you. Goodbye.")
+    def main():
+        conversation()
---
+++ js_project_06.py
@@ -0,0 +1,16 @@
+def conversation():
+    print("Hello, today we'll be doing number conversion.")
+    print("Choose the following number conversions")
+    (1) binary to decimal
+    (2) decimal to binary
+    (3) binary to hexadecimal
+    (4) decimal to hexadecimal
+    (5) hexadecimal to decimal
+    (6) hexadecimal to binary
+    (7) octal to decimal
+    (8) octal to hexadecimal
+
+    print("Converting Binary to Decimal")
+    print("Enter a valid Binary number")
+    ['0' and '1' are valid numbers]
+    print("Hit enter to continue or type 'yes' to end program")
